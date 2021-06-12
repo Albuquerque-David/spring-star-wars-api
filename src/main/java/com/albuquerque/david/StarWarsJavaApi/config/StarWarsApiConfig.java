761cbbef7a93a3e4845bff4535b5eec10a2c3cb7
David Albuquerque <davidra_rj@hotmail.com>
2021-06-12 17:09:53 -0300

    Added cors mapping

--- src/main/java/com/albuquerque/david/StarWarsJavaApi/config/StarWarsApiConfig.java
+++ src/main/java/com/albuquerque/david/StarWarsJavaApi/config/StarWarsApiConfig.java
@@ -1,4 +1,15 @@
 package com.albuquerque.david.StarWarsJavaApi.config;
 
-public class StarWarsApiConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class StarWarsApiConfig implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+
 }
